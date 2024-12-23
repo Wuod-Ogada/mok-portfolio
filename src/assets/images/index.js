36b9dc6e53b2169bf88e2a2edab268df99ed8e26
Klinsmann Ouma <oumamalaki@gmail.com>
2024-12-23 10:32:35 +0300

    first commit

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,14 +3,6 @@ import rating3 from './rating3.png';
 import rating4 from './rating4.png';
 
 
-import ohs from './ohs.jpg';
-import driver from './driver.png';
-import supportrep from './supportrep.jpg';
-
-import gear from './gear.jpg';
-import trashcan1 from './trashcan1.png';
-import wallet from './wallet.jpg';
-
 import laki1 from './laki1.png';
 import laki11 from './laki11.png';
 import laki11copy from './laki11.copy.png';
@@ -18,4 +10,4 @@ import laki2 from './laki2.png';
 import laki22 from './laki2.png';
 
 
-export { rating2, rating3, rating4, ohs, driver, supportrep, gear, trashcan1, wallet, laki1, laki11, laki2, laki22, laki11copy }
+export { rating2, rating3, rating4, laki1, laki11, laki2, laki22, laki11copy }
