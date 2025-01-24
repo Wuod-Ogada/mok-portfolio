475439a94ff39c0ebfd0c09f970cfad40e175539
Klinsmann Ouma <oumamalaki@gmail.com>
2025-01-24 15:27:22 +0300

    .env created and data imported

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -4,14 +4,16 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const ContactMe = () => {
 
+  const accesskey= import.meta.env.VITE_CONTACT_ACCESS_KEY
+  
   const [result, setResult] = React.useState("");
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setResult("Sending....");
     const formData = new FormData(event.target);
-
-    formData.append("access_key", "531c8cee-b894-423c-8ca6-7b94a53c2911");
+  
+    formData.append("access_key", accesskey);
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
@@ -34,18 +36,19 @@ const ContactMe = () => {
   return (
     <section className="section">
       <ToastContainer />
-
+      
       <div className="" id="Contact_me">
         <h2 className="section-title">Contact <span>Me!</span></h2>
         <div className=" ">
           <form onSubmit={onSubmit} className="mx-5 grid grid-flow-row gap-4 md:w-3/6 md:mx-auto">
             <div>
-              <input type="hidden" name="access_key" value="531c8cee-b894-423c-8ca6-7b94a53c2911"/>
+              <input type="hidden" name="access_key" value='{accesskey || ""}' />
             </div>
             <div className="grid grid-flow-row gap-1 ">
               <label htmlFor="name">Your Name:</label>
               <input type="text" placeholder="Full Name" name="name" className="h-[45px] px-3 text-[#0f112e] rounded-md" required/>
             </div>
+            
             <div className="flex flex-col gap-1">
               <label htmlFor="email">Email:</label>
               <input type="email" placeholder="Enter your Email" name="email" className="h-[45px] px-3 text-[#0f112e] rounded-md" required/>
