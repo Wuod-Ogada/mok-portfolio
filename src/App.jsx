94f0822de8d4b96efd296f70c186ecbfdc245c5c
Klinsmann Ouma <oumamalaki@gmail.com>
2025-01-29 13:16:32 +0300

    Testing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import NavHeader from './components/NavHeader';
+import Footer from './components/Footer';
+
 import Home from './pages/Homepage';
 import ExperiencePage from './pages/ExperiencePage';
 import ViewProject from './pages/ViewProjectPage';
-import MainLayout from './layouts/MainLayout';
+// import MainLayout from './layouts/MainLayout';
 import WorkWithMePage from './pages/WorkWithMePage';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-
+    <NavHeader /> 
           <Routes>
-            <Route path='/' element={<MainLayout />}>
+            <Route>
               <Route index element={<Home />} /> 
               <Route path="/work_experience" element={<ExperiencePage />} />
               <Route path="/Project/:title" element={<ViewProject />} />
               <Route path='/define_the_project' element={<WorkWithMePage />} />
             </Route>   
           </Routes>
-
+    <Footer />
       </BrowserRouter>
     </>
   )
